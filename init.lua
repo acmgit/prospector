087570c44c98aebd522c29057c07fc8892423c08
A.C.M <undertakers_help@yahoo.com>
2020-03-23 01:54:20 +0100

    Old commented Code deleted.

--- init.lua
+++ init.lua
@@ -103,59 +103,20 @@ function prospector.register_help(entry)
 
 end
 
---[[
-function prospector.show_nodelist(pattern)
-    if(pattern == "") then
-        prospector.print(prospector.green .. "Show the Nodelist:\n")
-        if(prospector.pnodelist ~= nil) then
-            for idx,entry in pairs(prospector.pnodelist)
-            do
-                prospector.print(prospector.yellow .. idx .. ": " ..
-                prospector.orange .. entry .. prospector.green .."\n")
+function prospector.split(parameter)
+    local cmd = {}
+    if(parameter ~= "" or parameter ~= nil) then
+        for word in string.gmatch(parameter, "[%w%-%:%_]+") do
+            table.insert(cmd, word)
 
-            end -- for _,key
+        end -- for word
 
-        else
-            prospector.print(prospector.red .. "Empty Nodelist.")
-            return
+    end -- if(parameter ~=
 
-        end -- if(prospector.pnodelist ~= nil
+    return cmd
 
-    else
-        prospector.print(prospector.green .. "Show the Nodelist only with: " ..
-        prospector.orange .. pattern .. prospector.green .. ".\n")
-        local count = 0
-        if(prospector.pnodelist ~= nil) then
-            for idx,entry in ipairs(prospector.pnodelist)
-            do
-                local hit = string.find(entry, pattern)
-                if(hit ~= nil) then
-                    prospector.print(prospector.yellow .. idx .. prospector.green .. ": " ..
-                    prospector.orange .. entry .. prospector.green .."\n")
-                    count = count + 1
+end -- function prospector.split
 
-                end
-
-            end -- for _,key
-
-        else
-            prospector.print(prospector.red .. "Empty Nodelist.")
-            return
-        end
-
-        if(count > 0) then
-            prospector.print(prospector.green .. "Found " .. prospector.yellow .. count ..
-            prospector.green .. " Nodes.\n")
-
-        else
-            prospector.print(prospector.light_red .. "No Nodes found.\n")
-
-        end -- if(count >
-
-    end -- if(pattern == ""
-
-end -- function(show_nodelist
-]]--
 
 function prospector.check_node(node)
     if(prospector.pnodelist ~= nil) then
@@ -244,20 +205,6 @@ function prospector.calc_distance_pos(pos_1, pos_2)
 
 end -- function calc_distance_pos
 
-function prospector.split(parameter)
-    local cmd = {}
-    if(parameter ~= "" or parameter ~= nil) then
-        for word in string.gmatch(parameter, "[%w%-%:%_]+") do
-            table.insert(cmd, word)
-
-        end -- for word
-
-    end -- if(parameter ~=
-
-    return cmd
-
-end -- function prospector.split
-
 function prospector.set_node(node)
     if(node == nil or node == "") then -- No Node is given
         if(prospector.current_Node == "") then -- No current_Node is set
@@ -309,181 +256,6 @@ function prospector.convert_position(pos)
 
     return nil
 end -- function convert_position
-
---[[
-    
-function prospector.pnode_lastpos(cmd)
-    local command = prospector.split(cmd)
-    if(command[1] == nil or command[1] == "") then
-        if(prospector.last_pos ~= "") then
-            prospector.print(prospector.green .. "The last found was at: " ..
-            prospector.orange .. prospector.last_pos ..
-            prospector.green .. ".\n")
-            prospector.print(prospector.green .. "This is "..
-            prospector.yellow .. prospector.calc_distance() ..
-            prospector.green .. " Nodes far away.\n")
-        else
-            prospector.print(prospector.red .. "There is no last Found set.\n")
-
-        end -- if(prospector.last_pos ~= nil
-
-    elseif(command[1] == "-s") then -- Sends the last Position to distancer
-
-        if(dst.ver == 2 and dst.rev >= 7) then
-            local mylastpos = prospector.last_pos
-            if(mylastpos == "") then
-                mylastpos = "0,0,0"
-
-            end -- if(mylastpos ==
-
-            dst.send_pos(minetest.localplayer:get_name(), mylastpos)
-
-        else
-            prospector.print(prospector.orange .. "No Distancer found or Version < 2.7.\n")
-
-        end -- if(dst.ver
-
-        if(prospector.distancer_channel ~= nil) then
-            if(prospector.distancer_channel:is_writeable()) then
-                prospector.distancer_channel:send_all("POS:" .. prospector.last_pos)
-                prospector.print(prospector.green .. "Position: " ..
-                prospector.orange .. prospector.last_pos .. prospector.green .. " send to Distancer.\n")
-
-            else
-                prospector.print(prospector.red .. "Modchannel not writeable.\n")
-
-            end -- if(prospector.distancer_channel:is_writeable
-
-        else
-            prospector.print(prospector.red .. "No Modchannel open.\n")
-            prospector.distancer_channel = minetest.mod_channel_join(prospector.distancer_channelname)
-
-        end -- if(prospector.distancer_channel
-
-    end -- if(command[1]
-
-end -- prospector.pnode_lastpos
-
-function prospector.pnode_search(cmd)
-    if(cmd == "" or cmd == nil) then
-        if(prospector.current_Node == "" or prospector.current_Node == nil) then
-            prospector.print(prospector.green .. "There is no searching Node set. Use command set_node <Nodename>.\n")
-            return
-
-        else
-            prospector.search_node(prospector.current_Node)
-            return
-
-        end --if(prospector.current_Node
-    end
-
-    local command = prospector.split(cmd)
-    if(command[1] == "-i") then -- command Index found
-        local idx = tonumber(command[2])
-        if(idx ~= nil) then -- valid Index
-            local node = prospector.pnodelist[idx]
-            if(node ~= nil or node ~= "") then -- valid Node found
-                prospector.search_node(node)
-                return
-
-            else
-
-                prospector.print(prospector.red .. "No Node found at Index " ..
-                prospector.yellow .. idx ..
-                prospector.red .. ".\n")
-                return
-
-            end -- if( node ~=
-
-        else
-            prospector.print(prospector.red .. "Invalid Indexnumber.\n")
-            return
-
-        end --(if(idx ~=
-
-    end -- if(command[1]
-
-    -- Param isn't empty and not the Command -i, so it's a Node
-    prospector.search_node(cmd)
-
-end -- prospector.pnode_search
-
-
-function prospector.pnode_setradius(cmd)
-    if(cmd == nil or cmd == "") then
-        if(prospector.searchRadius ~= nil or prospector.searchRadius ~= "") then
-            prospector.print(prospector.green .. "Current Radius is set to: " ..
-            prospector.orange .. prospector.searchRadius ..
-            prospector.green .. ".\n")
-            return
-        else
-            prospector.print(prospector.red .. "Illegal Radiusnumber set. Set a new Number.\n")
-            return
-
-        end -- if(prospector.searchRadius ~= nil
-
-    end -- if(cmd == nil
-
-    local radius = tonumber(cmd:trim())
-    prospector.print(prospector.green .. " Current Radius = " ..
-    prospector.orange .. prospector.searchRadius ..
-    prospector.green .. ".\n")
-    prospector.print(prospector.green .. " Max Radius = " ..
-    prospector.red .. prospector.maxRadius ..
-    prospector.green .. ".\n")
-
-    if(radius ~= nil and radius > 0 and radius <= prospector.maxRadius) then
-        prospector.searchRadius = radius
-        prospector.print(prospector.green .. " New Radius set to " ..
-        prospector.yellow .. prospector.searchRadius ..
-        prospector.green ..".\n")
-
-    else
-        prospector.print(prospector.red .. "Illegal Radiusnumber.\n")
-
-    end -- if(radius ~= nil
-
-end -- prospector.pnode_setradius
-
-function prospector.pnode_set(cmd)
-    local command = prospector.split(cmd)
-
-    -- No Node or Index given
-    if(command[1] == nil or command[1] == "") then
-        prospector.set_node("")
-        return
-
-    end -- if(command[1] == nil
-
-    -- Command Index found
-    if(command[1] == "-i") then
-        local idx = tonumber(command[2])
-
-        if(idx ~= nil) then
-            local node = prospector.pnodelist[idx]
-            if(node ~= nil or node ~= "") then
-                prospector.set_node(node)
-            return
-
-            else -- No Node at this Index found.
-                prospector.print(prospector.red .. "Wrong Indexnumber.\n")
-                return
-
-            end -- if(node ~= nil
-
-        else -- Illegal Index found, should not happen
-            prospector.print(prospector.red .. "Illegal Indexnumber entered.")
-            return
-
-        end -- if(idx ~nil)
-
-    end -- if(command[1]
-
-    -- No empty Param, no Index found, so it's a Node and set it
-    prospector.set_node(command[1])
-
-end -- prospector.pnode_set
-]]--
 
 function prospector.show_version()
     print("[CSM-MOD]" .. prospector.modname .. " v " ..
@@ -550,93 +322,6 @@ end -- distancer.handle_message
    ****************************************************************
 --]]
 
---[[
-minetest.register_chatcommand("pnode_lastpos", {
-
-    params = "<> | -s",
-    description = "Shows you the last Position of a found Node."..
-                  "\nUsage:"..
-                  "\n<> shows you the last Position."..
-                  "\n-s Sends the last Position to Distancer",
-    func = function(param)
-        local cmd = param:lower()
-        prospector.pnode_lastpos(cmd)
-
-    end -- function
-
-}) -- chatcommand prospector_last_pos
-
-
-minetest.register_chatcommand("pnode_list", {
-
-    params = "<> | <searchpattern>",
-    description = "Shows you all successfully found Nodes."..
-                  "\nUsage:"..
-                  "\n<> Shows you the whole Nodelist with Index."..
-                  "\n<searchpattern> Shows you a filtered Nodelist with <searchpattern>.\n",
-    func = function(param)
-        if(param == nil) then param = "" end
-        local cmd = param:lower()
-        prospector.show_nodelist(cmd)
-
-    end -- function
-
-}) -- chatcommand prospector_show_nodelist
-
-minetest.register_chatcommand("pnode_search", {
-
-    params = "<> | <Node> | <-i> index",
-    description = "Shows you the given Nodes in a Radius of <.set_radius>."..
-                  "\nUsage:"..
-                  "\n<> searches for the set node with command .set_node."..
-                  "\n<Node> search for <Node>"..
-                  "\n<-i> index searches for Node in the Nodelist.\n",
-    func = function(param)
-        local cmd = param:lower()
-        prospector.pnode_search(cmd)
-
-    end -- function
-
-}) -- chatcommand prospector_search_node
-
-minetest.register_chatcommand("pnode_setradius", {
-
-    params = "<> | <radius>",
-    description = "Set's or shows you the the Radius for the command .search_for."..
-                  "\nUsage:"..
-                  "\n<> Shows you the current Radius."..
-                  "\n<radius> set's a new Radius if valid.\n",
-    func = function(param)
-        prospector.pnode_setradius(param)
-
-    end -- function
-
-}) -- chatcommand prospector_set_radius
-
-minetest.register_chatcommand("pnode_set", {
-
-    params = "<> | <Node> | <-i> index",
-    description = "Set's a new Node for search."..
-                  "\nUsage:"..
-                  "\n<> shows the current Node for search."..
-                  "\n<Node> set's a new Node."..
-                  "\n<-i> index set's a new Node for search from the Nodelist.\n",
-    func = function(param)
-        local cmd = param:lower()
-        print(cmd)
-        prospector.pnode_set(cmd)
-
-    end -- func
-
-}) -- chatcommand prospector_set_node
-]]--
-
---[[
-   ****************************************************************
-   *******        Registered Chatcommands                    ******
-   ****************************************************************
---]]
-
 minetest.register_chatcommand("prs",{
     param = "<command> <parameter>",
     description = "Gives the Prospector a command with or without Parameter.\n",
@@ -650,8 +335,6 @@ minetest.register_chatcommand("prs",{
             end -- function
 
 }) -- minetest.register_chatcommand("dis
-
-
 
 --[[
    ****************************************************************
